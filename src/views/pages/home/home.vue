b7ee5abba6fc51ff8dc3317d92564987afe15f70
Einfach Valle <34622900+EinfachValle@users.noreply.github.com>
2023-03-28 21:28:40 +0200

    add pages + create router

---
+++ src/views/pages/home/home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="Home">
+    <h1>Homee</h1>
+  </div>
+</template>
+
+<script>
+import Home from './home';
+
+export default Home;
+</script>
+
+<style lang="scss" scoped>
+@import './home.scss';
+</style>
