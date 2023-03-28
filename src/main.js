0fb2595cdf8ed9de6e0886264953b724dd3fea6f
Einfach Valle <34622900+EinfachValle@users.noreply.github.com>
2023-03-28 21:15:02 +0200

    delete all old files + init vue-i18n

--- src/main.js
+++ src/main.js
@@ -1,14 +1,48 @@
-import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createApp } from 'vue'
+import App from './components/app/app.vue';
+import router from './router';
+import log from 'js-logger';
+import moment from 'moment-timezone';
+import i18n from './i18n';
+import axios from 'axios';
+import Particles from 'vue3-particles';
+import installI18n from './lang/index'
+// import redis from 'redis';
 
-import App from './App.vue'
-import router from './router'
+const app = createApp(App);
+app.use(i18n);
+app.use(moment);
+app.use(router);
+app.use(Particles);
+app.use(createPinia());
+app.mount('#app');
 
-import './assets/main.css'
+log.useDefaults();
 
-const app = createApp(App)
+const ahScriptURL = document.createElement('a');
+try {
+  ahScriptURL.href = document.querySelector('script[data-ahscript="neatbeat"]').src;
+} catch (e) {
+  // will use location.origin as fallback
+}
+const baseURL = ahScriptURL.origin || document.location.origin;
 
-app.use(createPinia())
-app.use(router)
+/* eslint-disable */
+if (
+  __webpack_require__ &&
+  typeof __webpack_require__.p === 'string' &&
+  __webpack_require__.p === ''
+) {
+  // eslint-disable-next-line camelcase
+  __webpack_require__.p = `${baseURL}/dist/`; // base src for all static resources
+}
+/* eslint-enable */
 
-app.mount('#app')
+axios.defaults.baseURL = `${baseURL}/api/`;
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+installI18n(app)
