3703a81a62a3d7a65ef1c86c759991b12d7efe56
Einfach Valle <34622900+EinfachValle@users.noreply.github.com>
2023-03-29 13:22:22 +0200

    update home js file

---
+++ src/views/pages/home/home.js
@@ -0,0 +1,20 @@
+
+export default {
+  name: 'HomePage',
+  data() {
+    return {
+
+    }
+  },
+  components: {},
+  props: {
+    toggleTheme: {
+      type: Function,
+      default: () => {},
+    },
+    currentTheme: {
+      type: Boolean,
+      default: () => true
+    },
+  },
+}
--- src/views/pages/home/home.vue
+++ src/views/pages/home/home.vue
@@ -1,15 +1,46 @@
 <template>
   <div id="Home">
-    <h1>Homee</h1>
+    <section class="sec1">
+      <div class="background-video">
+        <div class="content">
+          <div class="text">
+            <div class="t1">
+              <h1>Die neue Art zum Musik machen</h1>
+            </div>
+            <div class="t2">
+              <h2>Die Plattform für Produzenten</h2>
+            </div>
+          </div>
+          <div class="buttons">
+            <ul>
+              <li>
+                <a href="#sec2">Mehr erfahren</a>
+              </li>
+              <li>
+                <router-link :to="`/signup`">Konto erstellen</router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="sec2">
+
+    </section>
+    <section class="sec3">
+
+    </section>
   </div>
 </template>
 
 <script>
-import Home from './home';
+  import Home from './home';
 
-export default Home;
+  export default Home;
+
 </script>
 
 <style lang="scss" scoped>
-@import './home.scss';
+  @import './home.scss';
+
 </style>
