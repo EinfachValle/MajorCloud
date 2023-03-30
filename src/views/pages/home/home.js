80d15e195ee3cae8f8da4ef40690d9f7eb78f4f5
Einfach Valle <34622900+EinfachValle@users.noreply.github.com>
2023-03-30 20:03:09 +0200

    start create section 2

--- src/views/pages/home/home.js
+++ src/views/pages/home/home.js
@@ -1,3 +1,4 @@
+import { register } from 'swiper/element/bundle';
 
 export default {
   name: 'HomePage',
@@ -16,5 +17,8 @@ export default {
       type: Boolean,
       default: () => true
     },
+  },
+  mounted() {
+    register();
   },
 }
--- src/views/pages/home/home.vue
+++ src/views/pages/home/home.vue
@@ -31,7 +31,25 @@
       </div>
     </section>
     <section id="sec2">
-      <h1>Section 2</h1>
+      <div class="content">
+        <div class="swiper"></div>
+        <div class="text-block">
+          <div class="text">
+            <div class="t1">
+              <h1>Finde deinen Artist</h1>
+            </div>
+            <div class="t2">
+              <h2>Gib deinen Text deine eigene Stimme. Durch unseren
+                Artists auf unserer
+                Plattform hast du die Möglichkeit für jede
+                Songrichtung den Richtigen Sänger zu finden.</h2>
+            </div>
+          </div>
+          <div class="buttons">
+
+          </div>
+        </div>
+      </div>
     </section>
     <section id="sec3">
       <h1>Section 3</h1>
@@ -40,6 +58,10 @@
 </template>
 
 <script>
+  import {
+    SwiperContainer,
+    SwiperSlide
+  } from 'swiper/element';
   import Home from './home';
 
   export default Home;
