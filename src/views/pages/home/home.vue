cac080384ed517c9c2bda0478038be0df21d5d19
Einfach Valle <34622900+EinfachValle@users.noreply.github.com>
2023-03-30 19:47:19 +0200

    section 1 complete

--- src/views/pages/home/home.vue
+++ src/views/pages/home/home.vue
@@ -1,34 +1,40 @@
 <template>
   <div id="Home">
-    <section class="sec1">
+    <section id="sec1">
       <div class="background-video">
-        <div class="content">
-          <div class="text">
-            <div class="t1">
-              <h1>Die neue Art zum Musik machen</h1>
-            </div>
-            <div class="t2">
-              <h2>Die Plattform für Produzenten</h2>
-            </div>
+      </div>
+      <div class="content">
+        <div class="text">
+          <div class="t1">
+            <h1>Die neue Art zum Musik machen</h1>
           </div>
-          <div class="buttons">
-            <ul>
-              <li>
-                <a href="#sec2">Mehr erfahren</a>
-              </li>
-              <li>
-                <router-link :to="`/signup`">Konto erstellen</router-link>
-              </li>
-            </ul>
+          <div class="t2">
+            <h2>Die Plattform für Produzenten</h2>
           </div>
+          <div class="hr">
+            <span class="line"></span>
+          </div>
+          <div class="t3">
+            <h1>NEATBEAT</h1>
+          </div>
+        </div>
+        <div class="buttons">
+          <ul class="button-list">
+            <li>
+              <a href="#sec2">Mehr erfahren</a>
+            </li>
+            <li>
+              <router-link :to="`/signin`">Konto erstellen</router-link>
+            </li>
+          </ul>
         </div>
       </div>
     </section>
-    <section class="sec2">
-
+    <section id="sec2">
+      <h1>Section 2</h1>
     </section>
-    <section class="sec3">
-
+    <section id="sec3">
+      <h1>Section 3</h1>
     </section>
   </div>
 </template>
