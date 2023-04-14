cad2d5edee52b640e450d757e007a030524ca9c2
Einfach Valle <34622900+EinfachValle@users.noreply.github.com>
2023-04-14 13:56:25 +0200

    chnage navigation routing links

--- src/views/components/navigation/Nav.js
+++ src/views/components/navigation/Nav.js
@@ -1,4 +1,16 @@
 import Home from '../../pages/home-pages/home/home.vue';
+import OrdersPage from '../../pages/home-pages/orders/orders.vue';
+import JobPage from '../../pages/home-pages/job/job.vue';
+import NewPublicPage from '../../pages/home-pages/newPublic/newPublic.vue';
+
+import SignInPage from '../../pages/account-create-pages/signIn/signIn.vue';
+import SignUpPage from '../../pages/account-create-pages/signUp/signUp.vue';
+
+import AgbPage from '../../pages/footer-pages/agb/agb.vue';
+import ImprintPage from '../../pages/footer-pages/imprint/imprint.vue';
+import ParentsInfoPage from '../../pages/footer-pages/parentsInfo/parentsInfo.vue';
+import PrivacyPage from '../../pages/footer-pages/privacy/privacy.vue';
+import RightOfRecallPage from '../../pages/footer-pages/rightOfRecall/rightOfRecall.vue';
 
 import loader from '../loader/Loader.vue';
 import Footer from '../footer/Footer.vue';
@@ -22,6 +34,16 @@ export default {
   },
   components: {
     Home,
+    OrdersPage,
+    JobPage,
+    NewPublicPage,
+    SignInPage,
+    SignUpPage,
+    AgbPage,
+    ImprintPage,
+    ParentsInfoPage,
+    PrivacyPage,
+    RightOfRecallPage,
     loader,
     Footer
   },
