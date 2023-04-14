7c8872923f7101a8d470a5cb300a664ad40c4afb
Einfach Valle <34622900+EinfachValle@users.noreply.github.com>
2023-04-14 13:55:56 +0200

    add pages to router

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,27 @@ import {
   createRouter
 } from "vue-router";
 
+// routes:
+
 // 404 Page:
 import Page404 from '../views/pages/pageNotFound/page404.vue';
 
-// routes:
+// home-pages:
 import HomePage from '../views/pages/home-pages/home/home.vue';
+import OrdersPage from '../views/pages/home-pages/orders/orders.vue';
+import JobPage from '../views/pages/home-pages/job/job.vue';
+import NewPublicPage from '../views/pages/home-pages/newPublic/newPublic.vue';
+
+// create-account-pages:
+import SignInPage from '../views/pages/account-create-pages/signIn/signIn.vue';
+import SignUpPage from '../views/pages/account-create-pages/signUp/signUp.vue';
+
+// footer-pages:
+import AgbPage from '../views/pages/footer-pages/agb/agb.vue';
+import ImprintPage from '../views/pages/footer-pages/imprint/imprint.vue';
+import ParentsInfoPage from '../views/pages/footer-pages/parentsInfo/parentsInfo.vue';
+import PrivacyPage from '../views/pages/footer-pages/privacy/privacy.vue';
+import RightOfRecallPage from '../views/pages/footer-pages/rightOfRecall/rightOfRecall.vue';
 
 const routes = [
   {
@@ -18,6 +34,56 @@ const routes = [
     path: '/home',
     name: 'HomePage',
     component: HomePage,
+  },
+  {
+    path: '/orders',
+    name: 'OrdersPage',
+    component: OrdersPage,
+  },
+  {
+    path: '/job',
+    name: 'JobPage',
+    component: JobPage,
+  },
+  {
+    path: '/new',
+    name: 'NewPublicPage',
+    component: NewPublicPage,
+  },
+  {
+    path: '/signin',
+    name: 'SignInPage',
+    component: SignInPage,
+  },
+  {
+    path: '/signUp',
+    name: 'SignUpPage',
+    component: SignUpPage,
+  },
+  {
+    path: '/agb',
+    name: 'AgbPage',
+    component: AgbPage,
+  },
+  {
+    path: '/imprint',
+    name: 'ImprintPage',
+    component: ImprintPage,
+  },
+  {
+    path: '/informations',
+    name: 'ParentsInfoPage',
+    component: ParentsInfoPage,
+  },
+  {
+    path: '/privacy',
+    name: 'PrivacyPage',
+    component: PrivacyPage,
+  },
+  {
+    path: '/rights',
+    name: 'RightOfRecallPage',
+    component: RightOfRecallPage,
   },
   {
     path: '/404',
