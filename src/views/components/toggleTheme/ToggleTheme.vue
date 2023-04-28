434b6470a25c9c26dd63e456d197450005f23865
Valentin Röhle <34622900+EinfachValle@users.noreply.github.com>
2023-04-28 21:58:06 +0200

    Feature/color switch (#11) (#12)
    * enable hot reloading
    
    * create and implement ColorSwitch button

---
+++ src/views/components/toggleTheme/ToggleTheme.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="ToggleTheme">
+    <div class="toggle">
+      <div v-if="currentTheme" @click="toggleTheme()" title="Change to Light" id="toggleBtn"><i
+          class="bi bi-brightness-high toggle"></i></div>
+      <div v-else @click="toggleTheme()" title="Change to Dark" id="toggleBtn"><i class="bi bi-moon toggle"></i></div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import ToggleTheme from './ToggleTheme';
+
+export default ToggleTheme;
+</script>
+
+<style lang="scss" scoped>
+@import './ToggleTheme.scss';
+</style>
