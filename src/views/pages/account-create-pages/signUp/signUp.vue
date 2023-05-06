ea1d9f6af6ef6309e9847a94b1af757e46dff5b4
Einfach Valle <34622900+EinfachValle@users.noreply.github.com>
2023-05-06 15:57:13 +0200

    add language for signup page

--- src/views/pages/account-create-pages/signUp/signUp.vue
+++ src/views/pages/account-create-pages/signUp/signUp.vue
@@ -6,42 +6,41 @@
           <i class="bi bi-person-circle"></i>
         </div>
         <div class="content-right">
-          <h1><b>Registrierung</b></h1>
-          <h2>Erstelle dein Benutzerkonto um Zugriff auf Neatbeat zu bekommen.</h2>
+          <div class="txt">
+            <h1><b>{{ $t('signup.sign') }}</b></h1>
+            <h2>{{ $t('signup.createMessage') }}</h2>
+          </div>
 
           <form method="post">
             <p>
-              <label for="email">Deine E-Mail Adresse</label>
+              <label for="email">{{ $t('signup.email') }}</label>
               <!-- <br> -->
               <input type="email" id="email" name="email" placeholder="example@example.com" required>
             </p>
 
             <p>
-              <label for="password">Dein Passwort</label>
+              <label for="password">{{ $t('signup.password') }}</label>
               <input type="password" placeholder="**********" name="password" id="password" required>
               <i class="bi bi-eye-slash" id="togglePassword"></i>
             </p>
 
             <p>
-              <label for="password-repeat">Dein Passwort Wiederholen</label>
+              <label for="password-repeat">{{ $t('signup.repeatPassword') }}</label>
               <input type="password" placeholder="**********" name="password-repeat" id="password-repeat" required>
               <i class="bi bi-eye-slash" id="togglePasswordRepeat"></i>
             </p>
 
-            <label for="remindPassword" class="checkboxTxt">Passwort Merken
+            <label for="remindPassword" class="checkboxTxt">{{ $t('signup.remindPassword') }}
               <input type="checkbox" id="remindPassword" name="remindPassword">
               <span class="checkmark"></span>
             </label>
 
-            <label for="assignPrivacy" class="checkboxTxt" id="lastChild">Ich stimme den Datenschutzbestimmungen sowie den Allgemeinen
-              Geschäftsbedingungen zu.
-              Dazu bestätige ich, dass ich über 16 Jahre Alt bin. Ich stimme zu, dass die Anmeldedaten über eine
-              SSL-Verbindung an NeatBeat übertragen werden.
+            <label for="assignPrivacy" class="checkboxTxt" id="lastChild">{{ $t('signup.agreePrivacy') }}
               <input type="checkbox" id="assignPrivacy" checked="checked" name="assignPrivacy" required>
               <span class="checkmark"></span>
             </label>
 
-            <button type="submit" id="submit" class="submit">Account Erstellen</button>
+            <button type="submit" id="submit" class="submit">{{ $t('signup.create') }}</button>
           </form>
         </div>
       </div>
